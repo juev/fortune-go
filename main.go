e1d81491e85a189f8f6a4fde4c6e71d1beb164bc
Denis Evsyukov <denis@evsyukov.org>
2019-05-09 18:41:25 +0300

    Added basic support for directories

--- main.go
+++ main.go
@@ -12,21 +12,27 @@ import (
 
 var (
 	filename = kingpin.Arg("filename", "Fortune filename").String()
+	dat      []byte
 )
 
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 
-	_, err := os.Stat(*filename)
+	fi, err := os.Stat(*filename)
 	if err != nil {
 		fmt.Println(*filename + " is not exist...")
 		os.Exit(1)
 	}
-	dat, err := ioutil.ReadFile(*filename)
-	if err != nil {
-		fmt.Println("Cannot open " + *filename + " filename...")
-		os.Exit(1)
+	if fi.Mode().IsRegular() {
+		dat, err = ioutil.ReadFile(*filename)
+		if err != nil {
+			fmt.Println("Cannot open " + *filename + " filename...")
+			os.Exit(1)
+		}
+	} else if fi.Mode().IsDir() {
+		fmt.Println(*filename + " is a directory...")
+		os.Exit(0)
 	}
 
 	fortunes := strings.Split(string(dat), "\n%\n")
