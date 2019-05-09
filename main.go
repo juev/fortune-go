3b807d70626117b20eb298e6afa79dbcf341510b
Denis Evsyukov <denis@evsyukov.org>
2019-05-09 18:09:16 +0300

    Implement simple fortune program

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
-func main () {
-	fmt.Println("Hello world!")
+var (
+	filename = kingpin.Arg("filename", "Fortune filename").String()
+)
+
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	_, err := os.Stat(*filename)
+	if err != nil {
+		fmt.Println(*filename + " is not exist...")
+		os.Exit(1)
+	}
+	dat, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		fmt.Println("Cannot open " + *filename + " filename...")
+		os.Exit(1)
+	}
+
+	fortunes := strings.Split(string(dat), "\n%\n")
+	length := len(fortunes)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	rnd := r1.Intn(length)
+
+	fmt.Println(fortunes[rnd])
 }
