393ca658c5f4baf3d117bf78e15a083538323407
Denis Evsyukov <denis@evsyukov.org>
2019-05-11 13:51:44 +0300

    Added basic support for directories

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	filename = kingpin.Arg("filename", "Fortune filename").String()
-	dat      []byte
+	filename = kingpin.Arg("filename", "Fortune filename").Default(".").String()
 )
 
 func main() {
@@ -21,24 +22,55 @@ func main() {
 
 	fi, err := os.Stat(*filename)
 	if err != nil {
-		fmt.Println(*filename + " is not exist...")
-		os.Exit(1)
+		log.Fatal(*filename + " is not exist...")
 	}
+	var dat []byte
 	if fi.Mode().IsRegular() {
 		dat, err = ioutil.ReadFile(*filename)
 		if err != nil {
-			fmt.Println("Cannot open " + *filename + " filename...")
-			os.Exit(1)
+			log.Fatal("Cannot open " + *filename + " filename...")
 		}
+		printFortune(string(dat))
 	} else if fi.Mode().IsDir() {
-		fmt.Println(*filename + " is a directory...")
-		os.Exit(0)
-	}
+		files, err := ioutil.ReadDir(*filename)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fortunes := strings.Split(string(dat), "\n%\n")
+		var forts []string
+		for _, file := range files {
+			if isFortune(file.Name()) {
+				forts = append(forts, file.Name())
+			}
+		}
+		println(forts)
+	}
+}
+
+func printFortune(data string) {
+	fortunes := strings.Split(data, "\n%\n")
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 	rnd := r1.Intn(len(fortunes))
 
 	fmt.Println(fortunes[rnd])
+
+}
+
+func isFortune(filename string) bool {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal(filename + " is not exist...")
+	}
+
+	if fi.Mode().IsRegular() {
+		fidat, err := os.Stat(filename + ".dat")
+		if err != nil {
+			log.Fatal(filename + ".dat is not exist...")
+		}
+		if fidat.Mode().IsRegular() {
+			return true
+		}
+	}
+	return false
 }
